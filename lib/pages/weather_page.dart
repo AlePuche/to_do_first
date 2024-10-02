a66f93da6a822df1a44f30fcbae20ad9d9c41242
AlejandroPuche <alepucheperez@gmail.com>
2024-10-02 13:46:55 +0200

    app clima acabada

--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import '../models/weather_model.dart';
+import '../services/weather_service.dart';
 
 class WeatherPage extends StatefulWidget {
   const WeatherPage({super.key});
@@ -8,9 +10,42 @@ class WeatherPage extends StatefulWidget {
 }
 
 class _WeatherPageState extends State<WeatherPage> {
+  final _weatherService = WeatherService('38c41484c252e1bbe9602e400afa7efe');
+  Weather? _weather;
+
+  _fetchWeather() async {
+    String cityName = await _weatherService.getCurrentCity();
+    try {
+      final _weather = await _weatherService.getWeather(cityName);
+      setState(() {
+        this._weather = _weather;
+      });
+    }
+    catch (error) {
+      print('Error fetching weather: $error');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchWeather();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(_weather?.cityName ?? 'Loading city...'),
+
+            Text('${_weather?.temperature.round()} + ºC'),        ],
+        )
+      )
+    );
   }
 }
 
